34ff036f113f9212de61f3b9d803dfbc64d6cb94
NpEric360 <95456795+NpEric360@users.noreply.github.com>
2023-10-24 05:52:22 -0400

    Update main_api_s3_upload.py

--- main_api_s3_upload.py
+++ main_api_s3_upload.py
@@ -53,14 +53,6 @@ s3= boto3.resource("s3", aws_access_key_id = access_key, aws_secret_access_key =
 #establish connection to s3 using AWS credentials
 s3_client = boto3.client("s3", aws_access_key_id = access_key, aws_secret_access_key = secret_key)
 
-#1. Create bucket
-def create_bucket():
-    try:
-        s3_client.create_bucket(Bucket=S3_BUCKET_NAME)
-    except Exception as e:
-        print(e)
-
-
 #2. Upload Data to S3 bucket
 def get_current_utc_time():
     current_time = datetime.utcnow()
